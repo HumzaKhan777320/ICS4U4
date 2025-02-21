c4a9a55132238e61fcf2a3be9b6ef3d927128097
HumzaKhan777320 <777320@pdsb.net>
2025-02-21 00:21:19 -0500

    Update FixMePlease.java

--- FixMePlease.java
+++ FixMePlease.java
@@ -1,3 +1,16 @@
+/*
+Name: Humza Saleem Khan 
+Student Number: 777320@pdsb.net
+Course Code: ICS4U4
+Assignment: Homework - Fixing Errors
+02/21/2025
+
+Note: I added some additional comments in the GetValues function
+to better underdtand the code as I was reviewing it
+
+With fixing error comments I also $pecified what type of
+error it is along with where i.e. ^syntax error 
+ */
 public class FixMePlease {
 
     public static void main(String[] args) {
@@ -16,23 +29,23 @@ public class FixMePlease {
     }
 
     
-    public static int[] GetValues(int max, int min)
+    public static int[] GetValues(int max, int min)//declaring GetValues function
     {
         int length = max + 1;//setting the length to max + 1 as the length should be 1 more than the max 
         //               ^logical error  
         
-        if (length < 0)
+        if (length < 0)//checking that the length is not 0
         {
-            return null;
+            return null;//returning null 
         }
         
-        int[] arr = new int[length];
+        int[] arr = new int[length];//creating array arr of type int to return
         
-        for (int i=0; i < length; i++)
+        for (int i=0; i < length; i++)//iterating i length times
         {
-            arr[i] = i;
+            arr[i] = i;//appending to return array arr
         }
         
-        return arr;
+        return arr;//returning arr
     }
 }
