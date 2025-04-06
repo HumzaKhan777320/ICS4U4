96ac65eac17ec88de600e27163d06516f88bd439
HumzaKhan777320 <777320@pdsb.net>
2025-04-05 23:24:52 -0400

    Update Beta.java

--- Beta.java
+++ Beta.java
@@ -9,7 +9,7 @@ public class Beta {//declaring main class of Beta file
         
         a.publicProp = 0;//trying to access the public int variable in Alpha with viewable results
         a.protectedProp=0;//trying to access the protected int variable in Alpha with viewable results
-        a.defaultProp=0;//trying to alter the unspecified access int variable in Alpha with viewable results
+        a.defaultProp=0;//trying to access the unspecified access int variable in Alpha with viewable results
         //a.privateProp = 0;   //Not visible as private int variable was unaccessable
     }//closing Beta constructor
 }//closing Beta class
