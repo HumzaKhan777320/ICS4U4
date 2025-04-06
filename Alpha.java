5e2135cd819c06636411818bbef2435e877c0436
HumzaKhan777320 <777320@pdsb.net>
2025-04-05 23:24:24 -0400

    Update Alpha.java

--- Alpha.java
+++ Alpha.java
@@ -8,4 +8,12 @@ public class Alpha {//creating a class to call in future constructors
     int defaultProp;//declaring a regular int variable without specification of access
     private int privateProp;//declaring a private int variable
 
+    public Alpha(){//creating a constructor for the Alpha file
+        Alpha a0 = new Alpha();//creating a new alpha object
+        a0.publicProp=0;//trying to access the public int variable in Alpha with viewable results
+        a0.protectedProp=0;//trying to access the protected int variable in Alpha with viewable results
+        a0.defaultProp=0;//trying to access the unspecified access int variable in Alpha with viewable results
+        a0.privateProp=0;//trying to access the private int variable in Alpha with viewable results
+    }//closing Alpha constructor
+
 }//closing Alpha class
